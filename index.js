a36e7e83cf749e66ee61cb96906c9e14dc5236e5
Mika Roosingh <Skeppo15@gmail.com>
2024-04-25 16:17:14 +0200

    d
    d

--- index.js
+++
@@ -1,8 +0,0 @@
-
-const navToggle = document.getElementById('navToggle');
-const mainNav = document.getElementById('mainNav');
-
-
-navToggle.addEventListener('click', function() {
-    mainNav.classList.toggle('show');
-});
